6ee27a9fd14b695b0a5eb353dbb07d969f742e0e
MrBurp <andreass.ziegler@gmail.com>
2022-11-19 20:06:22 +0100

    connecting javascript

---
+++ script.js
@@ -0,0 +1,4 @@
+const counter = document.querySelector(".counter");
+const nums = document.querySelectorAll(".nums span");
+const finalMessage = document.querySelector(".final");
+const replay = document.getElementById("replay");
