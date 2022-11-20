9bb6a25a4f3791b480278febf11bc238e755adcc
MrBurp <andreass.ziegler@gmail.com>
2022-11-20 18:13:12 +0100

    run  Animation fcn

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const counter = document.querySelector(".counter");
 const nums = document.querySelectorAll(".nums span");
 const finalMessage = document.querySelector(".final");
 const replay = document.getElementById("replay");
+
+runAnimation();
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    console.log(num, idx);
+  });
+}
